e18f25e6aa4cac2ad0b405ef6a929c8e3c45825a
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2026-06-19 04:59:17 +0200

    fix: Navbar display on /play

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+const hideNavbar = computed(() => route.path === "/play" || route.path.startsWith("/play/"));
+</script>
+
 <template>
   <div class="relative min-h-screen w-full flex flex-col font-sans">
-    <AppNavbar />
+    <AppNavbar v-if="!hideNavbar" />
     <main class="flex-1 w-full">
       <slot />
     </main>
