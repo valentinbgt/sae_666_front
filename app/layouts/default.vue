7d98ab6df4ecc5013b545719a4ff34afb5a1994a
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2026-06-19 09:09:03 +0200

    style: Removed footer on /play

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 const route = useRoute();
-const hideNavbar = computed(() => route.path === "/play" || route.path.startsWith("/play/"));
+const hideChrome = computed(() => route.path === "/play" || route.path.startsWith("/play/"));
 </script>
 
 <template>
   <div class="relative min-h-screen w-full flex flex-col font-sans">
-    <AppNavbar v-if="!hideNavbar" />
+    <AppNavbar v-if="!hideChrome" />
     <main class="flex-1 w-full">
       <slot />
     </main>
-    <AppFooter />
+    <AppFooter v-if="!hideChrome" />
   </div>
 </template>
